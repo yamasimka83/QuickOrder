a095ed257c9ad349a4da8088633681e8e893dffa
Maksim Lukyanenko <yamasimka83@gmail.com>
2020-09-01 11:49:21 +0300

    Edited popup

--- view/frontend/web/js/view/popup.js
+++ view/frontend/web/js/view/popup.js
@@ -15,7 +15,32 @@ require(
                 text: $.mage.__('Continue'),
                 class: 'mymodal1',
                 click: function () {
+
                     this.closeModal();
+
+
+
+                    return $.ajax({
+                        url: "http://magento24.loc/quick/post",
+                        type: 'POST',
+                        data: {
+                            'name': $('#name').val(),
+                            'email': $('#email').val(),
+                            'sku': $('#sku').val(),
+                            'phone': $('#phone').val(),
+                            'qty': $('#qty').val(),
+                            'comment': $('#comment').val(),
+                        },
+                        dataType: 'json',
+                        success: function(data) {
+
+                            console.log(data);
+                        },
+                        error: function(result) {
+                            console.log('no response !');
+                        }
+                    });
+
                 }
             }]
         };
