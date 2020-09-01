796f6f1e1e521d14e12ab95c52338765e8c54189
Maksim Lukyanenko <yamasimka83@gmail.com>
2020-09-01 13:46:16 +0300

    Added admin controller

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Lukas\QuickOrder\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ */
+class Index extends Action implements HttpGetActionInterface
+{
+    public const MENU_ID = 'Lukas_QuickOrder::index';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu(static::MENU_ID);
+
+        return $resultPage;
+    }
+}
